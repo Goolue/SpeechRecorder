037a5920b9759f16b3c9d8f314fd708dfc4b8b3b
Oded <oded.rojtblat@gmail.com>
2018-11-29 11:36:50 +0200

    created config file

---
+++ config.py
@@ -0,0 +1,10 @@
+mic_name = 'HDA Intel PCH: ALC3223 Analog (hw:1,0)'  # name of microphone to be used
+
+recognizer_lang = 'he'  # speech language to use
+recognizer_phrase_time_limit = 10  # maximum number of seconds a sentence can be
+
+# server parameter configs
+server_url = '127.0.0.1'
+server_port = 5000
+device_number = 0
+user_name = 'Oded'
--- listener.py
+++ listener.py
@@ -1,5 +1,6 @@
 import sys
 
+import config
 import speech_recognition as sr
 from speech_recognition import Microphone
 
@@ -14,7 +15,7 @@ def listening_callback(recognizer: sr.Recognizer, audio) -> None:
     try:
         # try to use Google speech recognition to extract the text.
         # the Google one works best (out of the free ones)
-        txt = recognizer.recognize_google(audio, language="he")
+        txt = recognizer.recognize_google(audio, language=config.recognizer_lang)
         print("text is:", txt)
         # TODO send to server here
     except sr.UnknownValueError:  # happens if the detected phrase is empty (= silence) or cannot be detected
@@ -23,25 +24,33 @@ def listening_callback(recognizer: sr.Recognizer, audio) -> None:
         print("Request error")
 
 
-mics_lst = sr.Microphone.list_microphone_names()
-mic = None
-for i, m in enumerate(mics_lst):
-    # TODO: change this str to be configurable
-    if m == 'HDA Intel PCH: ALC3223 Analog (hw:1,0)':
-        print(m, i)
-        mic = Microphone(device_index=i)
+def start_listening() -> None:
+    """
+    Listen for speech continuously, send each sentence to a speech-to-text converter, then send the text to the server.
+    Sentences are detected by pauses in the speech.
+    """
 
-if mic is None:
-    raise Exception("No microphone can be found!")
+    # look for the microphone specified in config file
+    mics_lst = sr.Microphone.list_microphone_names()
+    mic = None
+    for i, m in enumerate(mics_lst):
+        if m == config.mic_name:
+            mic = Microphone(device_index=i)
+    if mic is None:
+        raise Exception("No microphone can be found!")
 
-print("listening")
-# stop_listening is a function that when called stops the background listening
-stop_listening = sr.Recognizer().listen_in_background(mic, callback=listening_callback, phrase_time_limit=10)
+    print("listening!\npress Enter to stop.")
+    # stop_listening is a function that when called stops the background listening
+    stop_listening = sr.Recognizer().listen_in_background(mic, callback=listening_callback,
+                                                          phrase_time_limit=config.recognizer_phrase_time_limit)
+    # wait for an input line. this is done tp prevent the script from exiting.
+    # on the actual Raspberry pi device, this will most likely not happen because the device will not be connected to a
+    # screen or keyboard.
+    for _ in sys.stdin:
+        print("stopping recording and speech recognition!")
+    stop_listening(wait_for_stop=False)
 
-# wait for an input line. this is done tp prevent the script from exiting
-for line in sys.stdin:
-    print("stopping")
 
-stop_listening(wait_for_stop=False)
+start_listening()
 
-print("Exiting!")
+print("Bye bye!")
