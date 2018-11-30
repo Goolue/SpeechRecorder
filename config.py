aae1d3b51167e1fe0a9c3d8387ee2ce83fe864bb
Oded <oded.rojtblat@gmail.com>
2018-11-30 13:11:17 +0200

    corrected ip

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ recognizer_phrase_time_limit = 10  # maximum number of seconds a sentence can be
 noise_adjustment_time = 0
 
 # server parameter configs
-server_url = 'http://127.0.0.1'
+server_url = 'http://192.168.1.6'
 server_port = '5000'
 device_id = 0
 user_name = 'Oded'
