db3d994f2674a889fd91d3cba7993efd73cec58a
Oded <oded.rojtblat@gmail.com>
2018-11-30 13:48:12 +0200

    and again

--- listener/listener.py
+++ listener/listener.py
@@ -9,8 +9,8 @@ stop_listening = utils.listen_continuously()
 # wait for an input line. this is done tp prevent the script from exiting.
 # on the actual Raspberry pi device, this will most likely not happen because the device will not be connected to a
 # screen or keyboard.
-for _ in sys.stdin:
-    utils.log("stopping recording and speech recognition!", remote=True)
-stop_listening(False)
+input("Press Enter to stop...")
+utils.log("stopping recording and speech recognition!", remote=False)
+stop_listening(True)
 
 print("Bye bye!")
