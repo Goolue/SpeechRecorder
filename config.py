e054d841a5d5c46c6a7356cef2026b1d11ed87ab
Oded <oded.rojtblat@gmail.com>
2018-11-30 11:57:42 +0200

    changed mic to microsoft

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-# mic_name = 'Microsoft LifeCam VX-800: USB Audio (hw:3,0)'  # name of microphone to be used
-mic_name = 'HDA Intel PCH: ALC3223 Analog (hw:1,0)'  # name of microphone to be used
+mic_name = 'Microsoft LifeCam VX-800: USB Audio (hw:3,0)'  # name of microphone to be used
+# mic_name = 'HDA Intel PCH: ALC3223 Analog (hw:1,0)'  # name of microphone to be used
 
 recognizer_lang = 'he'  # speech language to use
 recognizer_phrase_time_limit = 10  # maximum number of seconds a sentence can be
