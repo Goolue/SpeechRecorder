ae6621f5d87872e2f6d01251313e5412523098df
Oded <oded.rojtblat@gmail.com>
2018-12-02 13:41:06 +0200

    added git pull to run_listener.sh

--- run_listener.sh
+++ run_listener.sh
@@ -1,5 +1,15 @@
 cd /home/pi/programming/SpeechRecorder
 echo "present working dir is:"
 pwd | cat
+echo ""
+
+echo "pulling git"
+git pull
+echo ""
+
+echo "git status is:"
+git status | cat
+echo ""
+
 echo "running the script now!"
 python -m listener.listener
